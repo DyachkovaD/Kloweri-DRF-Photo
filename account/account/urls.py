a664c49bed9c7ab8c64013350ee88096e3177030
Darya Dyachkova <d.dyachkova@bk.ru>
2024-10-24 21:13:50 +0300

    second

--- account/account/urls.py
+++ account/account/urls.py
@@ -16,6 +16,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
 from rest_framework.routers import SimpleRouter
 from achievements.views import ImagesViewset
 
@@ -25,5 +26,8 @@ router.register(r'images', ImagesViewset, basename='images')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/v1/', include(router.urls))
+    path('api/', include(router.urls)),
+    path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
+    path('api/docs/', SpectacularSwaggerView.as_view(url_name='schema'), name='docs'),
+
 ]
--- account/achievements/models.py
+++ account/achievements/models.py
@@ -2,10 +2,8 @@ from django.db import models
 
 
 class AccountPhoto(models.Model):
-    user_id = models.UUIDField(null=True, blank=True)
+    user_id = models.UUIDField(primary_key=True)
     image = models.ImageField(upload_to='media')
 
     def __str__(self):
         return self.user_id
-
-
